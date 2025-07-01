3dbd140518b4d5448bf0f73bc88630ed33198b29
Oducity <henry.odumamwen@gmail.com>
2025-07-01 17:19:06 +0100

    Completed the game without the loop and the random genertor.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -7,16 +7,32 @@ class Program
         Console.WriteLine("Hello World! This is the Exercise3 Guess My Number Game Project.");
 
         Console.Write("What is the magic number? "); // request for input from the player.
-        int guessNumber = int.Parse(Console.ReadLine()); //convert the user input to an int and store in a variable guessNumber.
+        int magicNumber = int.Parse(Console.ReadLine()); //convert the user input to an int and store in a variable guessNumber.
+
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        if (guess > magicNumber)
+        {
+            Console.WriteLine("Guess lower!");
+        }
+        else if (guess < magicNumber)
+        {
+            Console.WriteLine("Guess higher!");
+        }
+        else
+        {
+            Console.WriteLine($"You guessed it: {guess}");
+        }
 
         //Random randomGenerator = new Random(); //Create Random object.
         //int number = randomGenerator.Next(1, 20); //Pick random number between 1 to 20 and store in a int variable number.
 
-        //while (number != 0)
-        //{
+            //while (number != 0)
+            //{
 
-        //}
-        //Console.WriteLine(number);
+            //}
+            //Console.WriteLine(number);
 
     }
 }
