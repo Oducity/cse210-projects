f0156abc75b314c23fe56ccb2c8bcadf10df300a
Oducity <henry.odumamwen@gmail.com>
2025-07-01 17:02:33 +0100

    Requested and stored user input, created Random object.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -6,5 +6,17 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Exercise3 Guess My Number Game Project.");
 
+        Console.Write("What is the magic number? "); // request for input from the player.
+        int guessNumber = int.Parse(Console.ReadLine()); //convert the user input to an int and store in a variable guessNumber.
+
+        Random randomGenerator = new Random(); //Create Random object.
+        int number = randomGenerator.Next(1, 20); //Pick random number between 1 to 20 and store in a int variable number.
+
+        while (number != 0)
+        {
+            
+        }
+        Console.WriteLine(number);
+
     }
 }
