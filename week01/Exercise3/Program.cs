52240344807d58807ea66ef2727bf4ff2b6122b2
Oducity <henry.odumamwen@gmail.com>
2025-07-01 18:44:20 +0100

    Added a loop that replay the game if the user reply yes.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,45 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Guess My Number Game Project.");
-
-        Random randomGenerator = new Random(); //Create Random object.
-        int magicNumber = randomGenerator.Next(1, 100); //Pick random number between 1 to 100 and store in an int variable magicNumber.
-
-        int guess = 0; // initialized guess to 0.
-        
-
-        while (magicNumber != guess) //Created a loop that keep looping till the comparison is correct.
+        string response;
+        do
         {
-            Console.Write("What is your guess? "); //requested for guess input from user.
-            guess = int.Parse(Console.ReadLine()); //converted user input and in int variable guess.
+            Console.WriteLine("Hello World! This is the Exercise3 Guess My Number Game Project.");
 
-            
+            Random randomGenerator = new Random(); //Create Random object.
+            int magicNumber = randomGenerator.Next(1, 100); //Pick random number between 1 to 100 and store in an int variable magicNumber.
 
-            // Compare guess with magicNumber.
-            if (guess > magicNumber)
+            int guess = 0; // initialized guess to 0.
+            int guessCount = 0;
+
+            while (magicNumber != guess) //Created a loop that keep looping till the comparison is correct.
             {
-                Console.WriteLine("Guess Lower!");
+                Console.Write("What is your guess? "); //requested for guess input from user.
+                guess = int.Parse(Console.ReadLine()); //converted user input and in int variable guess.
+                guessCount += 1;
+
+
+                // Compare guess with magicNumber.
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Guess Lower!");
+                }
+
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Guess higher!");
+                }
+
+                else
+                {
+                    Console.WriteLine($"You guessed it: you guessed {guessCount} times.");
+                }
             }
+            Console.Write("Do you want to repay? ");
+            response = Console.ReadLine();
 
-            else if (guess < magicNumber)
-            {
-                Console.WriteLine("Guess higher!");
-            }
-
-            else
-            {
-                Console.WriteLine($"You guessed it: {guess}");
-            }
-        }
-
-        
-
-            //while (number != 0)
-            //{
-
-            //}
-            //Console.WriteLine(number)
-
+        } while (response == "yes");
     }
 }
