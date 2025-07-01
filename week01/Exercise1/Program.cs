2b58a0f6f35926ac84d24d5b82311f24a4fe1b07
Oducity <henry.odumamwen@gmail.com>
2025-07-01 12:25:07 +0100

    completed exercise01 of week01.

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        Console.Write("What is your first name? "); //Request the user first name
+        string firstName = Console.ReadLine(); // Define string variable firstName, read and store user input into the string variable.
+
+        Console.Write("What is your last name? "); //Requested for user last name.
+        string lastName = Console.ReadLine(); // Define string variable lastName, read and store user input into the string variable.
+
+        Console.WriteLine($"Your names is {lastName}, {firstName} {lastName}.");
     }
 }
