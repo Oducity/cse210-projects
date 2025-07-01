37e724e243ff5f7a5843cc0d0a59160edc412966
Oducity <henry.odumamwen@gmail.com>
2025-07-01 17:06:03 +0100

    Comment out the Random object and the while loop.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -9,14 +9,14 @@ class Program
         Console.Write("What is the magic number? "); // request for input from the player.
         int guessNumber = int.Parse(Console.ReadLine()); //convert the user input to an int and store in a variable guessNumber.
 
-        Random randomGenerator = new Random(); //Create Random object.
-        int number = randomGenerator.Next(1, 20); //Pick random number between 1 to 20 and store in a int variable number.
+        //Random randomGenerator = new Random(); //Create Random object.
+        //int number = randomGenerator.Next(1, 20); //Pick random number between 1 to 20 and store in a int variable number.
 
-        while (number != 0)
-        {
-            
-        }
-        Console.WriteLine(number);
+        //while (number != 0)
+        //{
+
+        //}
+        //Console.WriteLine(number);
 
     }
 }
