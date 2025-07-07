4f62514ad73937ac8bfdd45467fc75e578408900
Oducity <henry.odumamwen@gmail.com>
2025-07-07 13:32:15 +0100

    Created the Resume class object, assigned _personalName value and added job1 and job2 to list of job.

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -18,5 +18,10 @@ class Program
         job2._jobTitle = "Lead Engineer";
         job2._startYear = 2022;
         job2._endYear = 2025;
+        //created the Resume class object.
+        Resume resume = new Resume();
+        resume._personalName = "Odumamwen Evans Henry Aisiokuebor";
+        resume._job.Add(job1); //Add job1 to the resume _job list.
+        resume._job.Add(job2); //Add job2 to the resume _job list.
     }
 }
