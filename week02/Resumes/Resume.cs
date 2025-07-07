554a48c99c6cd77f286619113c72d82388e580fb
Oducity <henry.odumamwen@gmail.com>
2025-07-07 13:44:46 +0100

    Added a loop in  DisplayResume that iterate the _job and display all jobs information.

--- week02/Resumes/Resume.cs
+++ week02/Resumes/Resume.cs
@@ -10,6 +10,11 @@ public class Resume
 
     public void DisplayResume()
     {
-        
+        Console.WriteLine($"{_personalName}");
+        Console.WriteLine("Jobs Experience:");
+        foreach (Job j in _job)
+        {
+            Console.WriteLine($"Company: {j._companyName}, Job Title: {j._jobTitle}, Start-Year: {j._startYear} - End-Year: {j._endYear}");
+        }
     }
 }
