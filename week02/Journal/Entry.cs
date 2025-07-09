e2313edefd9b1628c9746114e7d889d5160608b1
Oducity <henry.odumamwen@gmail.com>
2025-07-09 21:47:29 +0100

    Display method added to the Entry class.

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -1,10 +1,17 @@
 public class Entry // Created the Entry class.
 {
+    // Created the Attributes
     public string _promptText;
     public string _entryText;
     public string _date;
 
+    //Created the class constructor.
     public Entry()
+    {
+
+    }
+
+    public void Display() // Display method added.
     {
 
     }
