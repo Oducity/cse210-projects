0792e968ac48cd2beb9c102db93857597caaffc3
Oducity <henry.odumamwen@gmail.com>
2025-07-09 22:49:36 +0100

    Added the Journal class object in the Program.cs file.

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new Journal();//Created Journal class object and assigned it, journal.
+        
     }
 }
