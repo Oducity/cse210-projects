259102b3477e5971e01b6bb036257f385c3d8e26
Oducity <henry.odumamwen@gmail.com>
2025-07-09 23:28:43 +0100

    Developed code to display user journal.

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -19,7 +19,33 @@ class Program
             Console.WriteLine("To Quit: Press 5");
             Console.Write("What do you want to do? ");//Take menu input from the user.
             menu = Convert.ToInt32(Console.ReadLine());//Re-assign the user input to the menu variable.
-            
+            string fileName;
+
+
+            // Comparison statement to check the menu value.
+            if (menu == 1)
+            {
+                PromptGenerator generator = new PromptGenerator();  // Created the PromptGenerator class object.
+                Entry entry1 = new Entry(); // Created the Entry class object.
+
+
+                entry1._promptText = generator.GetRandomPrompt();  //Get random question from the GetRandomPrompt method and store it in the _promptText attribute in the Entry class.
+                entry1._entryText = Console.ReadLine();  // Store the user response into the _entryText attribute in the Entry class.
+                DateTime currentDate = DateTime.Now; // Get the current date from the computer and store it in the currentDate variable.
+                entry1._date = currentDate.ToShortDateString(); // Re-assign the date to the _date attribute in the Entry class.
+                journal.AddEntry(entry1); // Calling the AddEntry method from the Journal class and adding all entries to the Entry list including the date.
+            }
+
+            else if (menu == 2)
+            {
+                Console.WriteLine("List in journal is:");
+                journal.DisplayAll();
+            }
+
+            else if (menu == 3)
+            {
+                
+            }
         }
     }
 }
