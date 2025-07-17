a8d3aa3897c5e9149223a5cc01bf2e4281159d8b
Oducity <henry.odumamwen@gmail.com>
2025-07-17 12:39:22 +0100

    Assibned all four attribute in the Reference class object constructor.

--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -1,4 +1,20 @@
 public class Reference
 {
-    
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _endVerse;
+
+    public Reference() // Object constructor with all four attributes assigned.
+    {
+        _book = "1 Nephi";
+        _chapter = 1;
+        _verse = 9;
+        _endVerse = 10;
+    }
+
+    public void GetReference()
+    {
+
+    }
 }
