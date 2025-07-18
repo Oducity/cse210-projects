7addd9d90a692e9abcfc8e1bf4d0e0a0fdb6075f
Oducity <henry.odumamwen@gmail.com>
2025-07-18 18:07:16 +0100

    Developed the fraction Program class, created all class objects and called all needed methods.

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine(fraction1.GetFractionToString()); //Print the string representation to the console.
+        Console.WriteLine(fraction1.GetDecimalValue()); // Print the decimal value to the console.
+
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine(fraction2.GetFractionToString()); //Print the string representation to the console.
+        Console.WriteLine(fraction2.GetDecimalValue()); // Print the decimal value to the console.
+
+        Fraction fraction3 = new Fraction(3, 4);
+        Console.WriteLine(fraction3.GetFractionToString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
+        Fraction fraction4 = new Fraction(1, 3);
+        Console.WriteLine(fraction4.GetFractionToString());
+        Console.WriteLine(fraction4.GetDecimalValue());
     }
 }
