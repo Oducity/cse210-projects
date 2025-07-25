f26d5e258cd376e79c16ce1072654d055ad95dbc
Oducity <henry.odumamwen@gmail.com>
2025-07-26 00:43:12 +0100

    Added the YouTubeAccount class and it attributes/constructor in its file.

--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -2,10 +2,11 @@ public class Video
 {
     public string _title;
     public string _author;
-    public List<Comment> _comments;
+    public double _length;
+    public List<Comment> _comments = new List<Comment>();
 
     public Video()
     {
-        
+
     }
 }
---
+++ week04/YouTubeVideos/YouTubeAccount.cs
@@ -0,0 +1,10 @@
+public class YouTubeAccount
+{
+    public string _accountName;
+    public List<Video> _videos = new List<Video>();
+
+    public YouTubeAccount()
+    {
+
+    }
+}
