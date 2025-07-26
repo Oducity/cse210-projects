5b7465e3376fbe375d4b1856563fdbeced214324
Oducity <henry.odumamwen@gmail.com>
2025-07-26 19:32:55 +0100

    Created the attributes in the Product class file.

--- week04/OnlineOrdering/Product.cs
+++ week04/OnlineOrdering/Product.cs
@@ -1,4 +1,12 @@
 public class Product
 {
-    
+    private string _name;
+    private double _id;
+    private double _price;
+    private double _quantity;
+
+    public Product()
+    {
+        
+    }
 }
