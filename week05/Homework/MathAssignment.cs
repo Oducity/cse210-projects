0e7b15750c67c7c57d6e17ba6140f0c7f0953e48
Oducity <henry.odumamwen@gmail.com>
2025-07-30 06:05:47 +0100

    Created MathAssignment class and its attributes and method in its file.

---
+++ week05/Homework/MathAssignment.cs
@@ -0,0 +1,15 @@
+public class MathAssignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment()
+    {
+
+    }
+
+    public string GetHomeWorkList()
+    {
+        return "";
+    }
+}
