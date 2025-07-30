5242b76dcf52a949d9772fb12b2a468d8ce64f56
Oducity <henry.odumamwen@gmail.com>
2025-07-30 07:13:46 +0100

    Changed the MathAssignment and WritingAssignment classes to derived classes and added their necessary parameters.

--- week05/Homework/MathAssignment.cs
+++ week05/Homework/MathAssignment.cs
@@ -1,11 +1,12 @@
-public class MathAssignment
+public class MathAssignment : Assignment
 {
     private string _textbookSection;
-    private string _problems;
+    private string _problem;
 
-    public MathAssignment()
+    public MathAssignment(String studentName, string topic, string textbookSection, string problem) : base(studentName, topic)
     {
-
+        _textbookSection = textbookSection;
+        _problem = problem;
     }
 
     public string GetHomeWorkList()
