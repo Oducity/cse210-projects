26aba2785f01507fa40facea9e45463b185e59c6
Oducity <henry.odumamwen@gmail.com>
2025-07-30 05:58:58 +0100

    Created base class in its file and added the attributes and method.

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,15 @@
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment()
+    {
+
+    }
+
+    public string GetSummary()
+    {
+        return "";
+    }
+}
