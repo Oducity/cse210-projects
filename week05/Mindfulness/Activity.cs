6c25f226b8585d413bd7b631628d2dc54066e5dc
Oducity <henry.odumamwen@gmail.com>
2025-08-01 12:39:19 +0100

    Created the Activity class as the base class, its attributes and methods in its file.

---
+++ week05/Mindfulness/Activity.cs
@@ -0,0 +1,22 @@
+public class Activity
+{
+    private string _activityName;
+    private string _description;
+    private DateTime _duration;
+    private DateTime _pauseTimer;
+
+    public Activity()
+    {
+
+    }
+
+    public void DisplayIntro()
+    {
+
+    }
+
+    public void DisplayEndMessage()
+    {
+        
+    }
+}
