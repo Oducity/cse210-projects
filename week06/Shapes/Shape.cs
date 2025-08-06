230e3e2ba865183eba831a73953f4a65cc55863f
Oducity <henry.odumamwen@gmail.com>
2025-08-06 12:15:58 +0100

    Developed the getters, setters and the GetArea method.

--- week06/Shapes/Shape.cs
+++ week06/Shapes/Shape.cs
@@ -1,4 +1,18 @@
 public class Shape
 {
-    
+    string _color;
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+    public void GetArea()
+    {
+
+    }
 }
